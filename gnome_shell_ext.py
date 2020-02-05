a0844b61c52c0f9646ee9d04f053c6ad960b8296
yut23 <yut23@gvljohnsons.com>
2020-02-05 15:27:31 -0500

    Update gnome_shell_ext.py for gnome-shell 3.34

--- gnome_shell_ext.py
+++ gnome_shell_ext.py
@@ -2,43 +2,28 @@
 Cribbed from gnome-shell-extension-tool.
 """
 
-from gi.repository import Gio
-
-ENABLED_EXTENSIONS_KEY = "enabled-extensions"
+import subprocess
 
 
 def enable_extension(uuid: str) -> None:
     """
     Enables the extension with `uuid`.
     """
-    settings = Gio.Settings(schema="org.gnome.shell")
-    extensions = settings.get_strv(ENABLED_EXTENSIONS_KEY)
-
-    if uuid not in extensions:
-        extensions.append(uuid)
-        settings.set_strv(ENABLED_EXTENSIONS_KEY, extensions)
+    subprocess.run(["/usr/bin/gnome-extensions", "enable", uuid], check=True)
 
 
 def disable_extension(uuid: str) -> None:
     """
-    Disables all extensions with `uuid`.
+    Disables the extension with `uuid`.
     """
-    settings = Gio.Settings(schema="org.gnome.shell")
-    extensions = settings.get_strv(ENABLED_EXTENSIONS_KEY)
-
-    if uuid in extensions:
-        # Use a while loop here to remove *all* mentions instances
-        # of the extension. Some faulty tools like to append more than one.
-        while uuid in extensions:
-            extensions.remove(uuid)
-
-        settings.set_strv(ENABLED_EXTENSIONS_KEY, extensions)
+    subprocess.run(["/usr/bin/gnome-extensions", "disable", uuid], check=True)
 
 
 def is_extension_enabled(uuid: str) -> bool:
     """
     Returns True if the extension with `uuid` is enabled.
     """
-    settings = Gio.Settings(schema="org.gnome.shell")
-    extensions = settings.get_strv(ENABLED_EXTENSIONS_KEY)
-    return uuid in extensions
+    output = subprocess.check_output(
+        ["/usr/bin/gnome-extensions", "info", uuid], text=True
+    )
+    return "State: ENABLED" in output
