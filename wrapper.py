690f0f6ccf4e91adeae657d7e2acc41f3fc2170a
yut23 <yut23@gvljohnsons.com>
2020-02-05 11:34:49 -0500

    Add handler to log uncaught exceptions

--- wrapper.py
+++ wrapper.py
@@ -96,6 +96,19 @@ LOGFILE_FORMATTER = logging.Formatter(
 )
 LOGFILES: Set[str] = set()
 
+
+# log any uncaught exceptions
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logger.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+
+sys.excepthook = handle_exception
+
+
 TIME_LOGFILE = None
 
 
