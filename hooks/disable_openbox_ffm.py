e6d8b35cf238a206648886b3addff09364f9cf0b
yut23 <yut23@gvljohnsons.com>
2021-07-15 23:07:48 -0400

    Add hook to disable focus-follows-mouse

---
+++ hooks/disable_openbox_ffm.py
@@ -0,0 +1,29 @@
+"""Disable focus-follows-mouse in Openbox."""
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+from . import WrapperHook, run
+
+
+class Hook(WrapperHook):
+    config_path = Path.home() / ".config/openbox/rc.xml"
+
+    def on_focus(self) -> None:
+        tree = ET.parse(self.config_path)
+        node = tree.find(
+            "./focus/followMouse", namespaces={"": "http://openbox.org/3.4/rc"}
+        )
+        if node is not None:
+            node.text = "no"
+            tree.write(self.config_path)
+            run(["openbox", "--reconfigure"])
+
+    def on_unfocus(self) -> None:
+        tree = ET.parse(self.config_path)
+        node = tree.find(
+            "./focus/followMouse", namespaces={"": "http://openbox.org/3.4/rc"}
+        )
+        if node is not None:
+            node.text = "yes"
+            tree.write(self.config_path)
+            run(["openbox", "--reconfigure"])
