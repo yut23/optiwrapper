002fabbcbdde03e132638f0bfac2d82ec8619338
yut23 <yut23@gvljohnsons.com>
2021-07-15 22:56:56 -0400

    Improve disable_touchpad
    It now uses my dedicated touchpad management tool, which allows it to
    prevent the touchpad from turning on automatically.

--- hooks/disable_touchpad.py
+++ hooks/disable_touchpad.py
@@ -1,19 +1,28 @@
-import re
+import fcntl
 
 from . import WrapperHook, check_output, run
+
+touchpad_cmd = "/home/eric/bin/touchpad"
+lock_file = "/var/lib/touchpad/disable.lock"
 
 
 class Hook(WrapperHook):
     def __init__(self) -> None:
         self.enabled = True
-        output = check_output(["/usr/bin/synclient", "-l"])
+        output = check_output([touchpad_cmd, "get"])
         # if touchpad is already disabled, don't re-enable it when the game stops
-        self.enabled = re.search(r"^TouchpadOff\s*=\s*1$", output) is None
+        self.enabled = "on" in output
+        # obtain a shared lock on the lock file, to block the touchpad from
+        # turning on when the mouse turns off.
+        self.fd = open(lock_file, "r")
+        fcntl.flock(self.fd, fcntl.LOCK_SH)
 
     def on_start(self) -> None:
         if self.enabled:
-            run(["/usr/bin/synclient", "TouchpadOff=1"], check=True)
+            run([touchpad_cmd, "off"], check=True)
 
     def on_stop(self) -> None:
+        fcntl.flock(self.fd, fcntl.LOCK_UN)
+        self.fd.close()
         if self.enabled:
-            run(["/usr/bin/synclient", "TouchpadOff=0"], check=True)
+            run([touchpad_cmd, "auto"], check=True)
