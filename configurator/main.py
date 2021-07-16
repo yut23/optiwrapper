8b0caed1046c043291893ce54e52c8225794e1c5
yut23 <yut23@gvljohnsons.com>
2021-07-15 23:42:15 -0400

    Add initial code for a GUI configuration tool

---
+++ configurator/main.py
@@ -0,0 +1,26 @@
+# type: ignore
+import sys
+
+from PySide2.QtCore import QFile
+from PySide2.QtWidgets import QApplication, QMainWindow
+
+from ui_settingswindow import Ui_SettingsWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_SettingsWindow()
+        self.ui.setupUi(self)
+        # keep game picker size fixed when resizing the window
+        self.ui.splitter.setStretchFactor(0, 0)
+        self.ui.splitter.setStretchFactor(1, 1)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
