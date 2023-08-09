974df07e653f1eba8e2d4c218a1edaf4ec0f7ae6
yut23 <yut23@gvljohnsons.com>
2023-08-08 20:17:22 -0400

    Fix logic in hooks.run() and .check_output()

--- optiwrapper/hooks/__init__.py
+++ optiwrapper/hooks/__init__.py
@@ -45,7 +45,7 @@ def run(
 
     The other arguments are the same as for the Popen constructor."""
     kwargs = kwargs.copy()
-    env_override = remove_overlay(is_32_bit)
+    env_override = remove_overlay(not is_32_bit)
     if "env" in kwargs:
         kwargs["env"].update(env_override)
     else:
@@ -60,7 +60,7 @@ run.__doc__ = subprocess.run.__doc__
 
 def check_output(*args: Any, is_32_bit: bool = False, **kwargs: Any) -> str:
     kwargs = kwargs.copy()
-    env_override = remove_overlay(is_32_bit)
+    env_override = remove_overlay(not is_32_bit)
     if "env" in kwargs:
         kwargs["env"].update(env_override)
     else:
